7a89aed2f2328dde7ec5d690f561f60cac9585f0
metherul <greenmarleyethan@gmail.com>
2016-12-19 02:24:27 -0500

    Added Google Maps static image functionalities
    Signed-off-by: metherul <greenmarleyethan@gmail.com>

--- NetworkTester/NetworkTester/MainWindow.xaml.cs
+++ NetworkTester/NetworkTester/MainWindow.xaml.cs
@@ -48,6 +48,9 @@ namespace NetworkTester
         private void btn_showAddFlyout_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             flo_addIp.IsOpen = true;
+
+            APIData apiData = LocationAPI.GetLocation("50.111.100.18");
+            GoogleMaps.FindImage(apiData.latitude, apiData.longitude);
         }
 
         public void OnAllPingsReceived(object source, PingEventArgs e)
