dbd98b7e91d82152ce2a587a044304921329ca32
Maxwell Wenger <carefreefour@gmail.com>
2016-12-18 23:17:41 -0800

    Added traceroot class

---
+++ NetworkTester/NetworkTester/Traceroute.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace NetworkTester
+{
+    public class Traceroute
+    {
+        private string address;
+        private const int Timeout = 2500;
+        private int maxHops;
+
+        public Traceroute(string address, int maxHops = 30)
+        {
+            maxHops = maxHops < 1 || maxHops > 100 ? maxHops : 30;
+            this.maxHops = maxHops;
+
+            this.address = Dns.GetHostAddresses(address)[0].ToString();
+        }
+
+        public List<PingExtensions.PingResult> GetRoute(int nextHop = 1)
+        {
+            PingExtensions.PingResult result;
+            using (var ping = new Ping())
+            {
+                var options = PingOptionsFactory(nextHop);
+
+                var p = ping.Send(address, Timeout, new byte[32], options);
+
+                result = PingResultFactory(nextHop, p);
+            }
+
+            List<PingExtensions.PingResult> results;
+            if (result.SourceAddress.Equals(result.DestinationAddress))
+            {
+                results = new List<PingExtensions.PingResult>
+                {
+                    result
+                };
+            }
+            else if (nextHop >= maxHops)
+            {
+                results = null;
+            }
+            else
+            {
+                results = GetRoute(++nextHop);
+            }
+
+            return results;
+        }
+
+        private PingExtensions.PingResult PingResultFactory(int nextHop, PingReply p)
+        {
+            var result = new PingExtensions.PingResult();
+
+            if (p != null)
+            {
+                result.SourceAddress = p.Address.ToString();
+                result.DestinationAddress = address;
+                result.Status = p.Status.ToString();
+                result.RoundtripTime = p.RoundtripTime;
+                result.TimeToLive = nextHop;
+            }
+            return result;
+        }
+
+        private static PingOptions PingOptionsFactory(int nextHop)
+        {
+            var options = new PingOptions
+            {
+                DontFragment = true,
+                Ttl = nextHop
+            };
+            return options;
+        }
+    }
+}
